e5cd2fc732decdc9d6edba2409a2bdb471eb454b
VathsalaM <vathsala@thoughtworks.com>
2016-05-17 18:09:04 +0530

    changed files

--- src/board.go
+++ src/board.go
@@ -17,13 +17,13 @@ type Element interface {
 	Kill()(bool)
 }
 
-func createTable(size int, length int) (map[int]map[int]tile) {
+func createTable(length int,width int ) (map[int]map[int]tile) {
 	colours := []string{ "white","black"}
 	columns := make(map[int](map[int]tile),0)
 	for j := 0; j < length; j++ {
 		var row = make(map[int]tile,0)
-		for i :=0; i < 8; i++ {
-			row[i] = NewTile((j*8)+i, colours[i % 2])
+		for i :=0; i < width; i++ {
+			row[i] = NewTile((j*width)+i, colours[i % 2])
 		}
 		columns[j] = row
 	}
--- src/tile_test.go
+++ src/tile_test.go
@@ -3,9 +3,21 @@ package src
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"github.com/VathsalaM/GoScrete/src/components"
 )
 
 func TestForTileColour(t *testing.T) {
 	newTile:=NewTile(1,"black")
 	assert.Equal(t,"black",newTile.color)
+}
+
+func TestCanPlace(t *testing.T) {
+	newSoldier := components.NewSoldier(1,"black")
+	newHorse := components.NewHorse(3,"white")
+	newTile:=NewTile(1,"black")
+	assert.Equal(t,Position{},newSoldier.Positon())
+	newTile.Place(newSoldier)
+	assert.Equal(t,Position{row:1,column:1},newSoldier.Positon())
+	newTile.Place(newHorse)
+	assert.Equal(t,Position{row:nil,column:nil},newSoldier.Positon())
 }
